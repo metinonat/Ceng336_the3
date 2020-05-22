4e88e5135fa188365fac90d4326ba93f57521dfc
onatceltik <64609605+onatceltik@users.noreply.github.com>
2020-05-22 03:03:03 +0300

    ADC task done, may need re-check

--- main.c
+++ main.c
@@ -1,18 +1,143 @@
 #pragma config OSC = HSPLL, FCMEN = OFF, IESO = OFF, PWRT = OFF, BOREN = OFF, WDT = OFF, MCLRE = ON, LPT1OSC = OFF, LVP = OFF, XINST = OFF, DEBUG = OFF
 
 #include <xc.h>
+#include "breakpoints.h"
+
+
+volatile int adc_value;
+volatile char special;
+bit adc_flag;
+bit rb4_flag;
+bit correct_guess_flag;
+bit end_game_flag;
+char temp_adc_high;
+char temp_adc_low;
+
+void init(){
+    
+    ADRESH = 0;
+    ADRESL = 0;
+    
+    ADCON0 = 0x31;  // channel 12 (AN12) : enable ADON
+    ADCON1 = 0;     // all pins analog : default Vrefs
+    ADCON2 = 0x82;  // ADFM set : Tacq = 0.Tad : Fosc/32
+    
+    ADIE = 0;       // ADC interrupt disable : timer0 will check that
+    TMR0IF = 0;     // disable for now
+    GODONE = 1;     // start conversion
+    adc_flag = 0;   // flag not set initially
+    
+    TRISH = 0;      // 
+    RH0 = 1;        // enable digit 0
+    
+    TMR0ON = 1;     // start timer0
+    return;
+}
+
+void __interrupt(low_priority) low_isr(){
+    if(TMR0IF == 1){
+        adc_flag      = 1;      // do adc_task in main
+        temp_adc_high = ADRESH; // get high bits
+        temp_adc_low  = ADRESL; // get low bits
+        GODONE        = 1       // start the conversion again
+        TMR0IF        = 0;      // clear timer0 interrupt flag
+    }
+    return;
+}
+
+void adc_task(){ // get ADRESH & ADRESL : 
+    
+    adc_flag = 0; // clear flag
+    adc_value = temp_adc_high;
+    adc_value << 8;
+    adc_value += temp_adc_low;
+    
+    if(0 <= adc_value || adc_value <= 102){ adc_value = 0; }
+    else if(adc_value <= 204) { adc_value = 1; }
+    else if(adc_value <= 306) { adc_value = 2; }
+    else if(adc_value <= 408) { adc_value = 3; }
+    else if(adc_value <= 510) { adc_value = 4; }
+    else if(adc_value <= 612) { adc_value = 5; }
+    else if(adc_value <= 714) { adc_value = 6; }
+    else if(adc_value <= 816) { adc_value = 7; }
+    else if(adc_value <= 918) { adc_value = 8; }
+    else if(adc_value <= 1023){ adc_value = 9; }
+    
+    return;
+}
+
+
+
+void check_RB4_button(){
+    
+    return;
+}
+
+char game_check(){
+    
+    return 0;
+}
+
+void blink_2_sec(){
+    /* initially turned on
+     * wait 500 ms then H=0, turn off
+     * wait 500 ms then H=1, turn on
+     * wait 500 ms then H=0, turn off
+     * wait 500 ms then H=1, turn on -> tartisalim
+     * */
+    return;
+}
+
+void latj_update(void){ // 7 Segment Number
+    switch(adc_value){
+        case 0  : LATJ = 11111100;
+        case 1  : LATJ = 01100000;
+        case 2  : LATJ = 11011010;
+        case 3  : LATJ = 11110010;
+        case 4  : LATJ = 01100110;
+        case 5  : LATJ = 10110110;
+        case 6  : LATJ = 10111110;
+        case 7  : LATJ = 11100000;
+        case 8  : LATJ = 11111110;
+        case 9  : LATJ = 11100110;
+        default : LATJ = 11111100;
+    }
+    return;    
+}
+
 
 void main(void) {
-    init_complete();
-    adc_value =5;
-    adc_complete();
-    rb4_handled();
-    latjh_update_complete();
-    latcde_update_complete();
-    correct_guess();
-    hs_passed();
-    game_over();
-    restart();
-    special_number();
+    
+    while(1){
+    
+        special = special_number();
+        
+        init();
+        init_complete(); // breakpoint
+        
+        while( !end_game_flag ){ // 5 second loop
+
+            if(adc_flag == 1){
+                adc_task();
+                adc_complete(); // breakpoint
+                latj_update();
+                latjh_update_complete(); // breakpoint
+            }
+            check_RB4_button();
+            if(rb4_flag == 1){
+                // other things
+                correct_guess_flag == 1;
+                rb4_handled();
+            }
+        }
+        // timer1 500 loading
+        if(correct_guess_flag == 0){ // initailly 0
+            game_over(); // breakpoint
+            latjh_update_complete(); // breakpoint
+        }
+        blink_2_sec();
+        restart(); // breakpoint
+    }
+    
     return;
 }
